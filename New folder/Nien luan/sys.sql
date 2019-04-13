f1772684e8f5f39d341db3d7d7d470e42f766373
nghiale1 <46536254+nghiale1@users.noreply.github.com>
2019-04-13 21:51:02 +0700

    đã lượt bỏ file trùng

--- New folder/Nien luan/sys.sql
+++
@@ -1,6 +0,0 @@
-create user nlcs identified by nlcs default tablespace users quota  5m on users
-
-grant create session to nlcs;
-grant create table to nlcs;
-grant create trigger to nlcs;
-grant create sequence to nlcs;
